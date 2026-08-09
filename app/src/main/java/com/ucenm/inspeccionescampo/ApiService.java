623472ea2221c2eb4e805b3777117cfd42a5bb3d
Diego Barahona <armandocastillo1516@gmail.com>
2026-08-08 18:14:26 -0600

    actualizado2.6

--- app/src/main/java/com/ucenm/inspeccionescampo/ApiService.java
+++ app/src/main/java/com/ucenm/inspeccionescampo/ApiService.java
@@ -12,12 +12,12 @@ import retrofit2.http.Part;
 
 public interface ApiService {
 
-    // 1. Enviar los datos generales y recibir la respuesta con el ID real
+    // 1. Enviar los datos generales de la inspección a inspecciones.php
     @POST("inspecciones.php")
-    Call<RespuestaServidor> enviarInspeccion(@Body Inspeccion inspeccion);
+    Call<Void> enviarInspeccion(@Body Inspeccion inspeccion);
 
     // 2. Obtener la lista de inspecciones desde MySQL
-    @GET("inspecciones.php")
+    @GET("GetPersons.php") // (O cámbialo a "inspecciones.php" si usas el mismo archivo para el GET)
     Call<List<Inspeccion>> obtenerInspecciones();
 
     // 3. Subir la FOTO al servidor PHP con su ID de inspección
